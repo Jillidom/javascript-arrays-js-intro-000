dddbfba957a147ff28ef77d594d2c523e0b5694b
Jillian Domingue <jmdomingue@gmail.com>
2020-05-04 01:44:54 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,7 +32,7 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 
-function remove ElementFromEndOfArray(array) {
+function removeElementFromEndOfArray(array) {
   return array.slice(0, array.length - 1)
 }
 
